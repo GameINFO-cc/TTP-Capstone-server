673ff825de236c2d075cd551e38ff9e9ccdcdf20
Rehan Khan <rehangh8924@gmail.com>
2020-06-18 01:39:36 -0400

    trying to setup google oauth

--- app.js
+++ app.js
@@ -1,91 +1,124 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const session = require('express-session')
+const passport = require('passport')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const db = require('./server/db')
+const sessionStore = new SequelizeStore({db})
+const PORT = process.env.PORT || 8080
+const app = express()
+const socketio = require('socket.io')
+module.exports = app
+
+// This is a global Mocha hook, used for resource cleanup.
+// Otherwise, Mocha v4+ never quits after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close the session store', () => sessionStore.stopExpiringSessions())
+}
+
 /**
- * Here, we will sync our database, create our application, and export this
- * module so that we can use it in the bin directory, where we will be able to
- * establish a server to listen and handle requests and responses;
+ * In your development environment, you can keep all of your
+ * app's secret API keys in a file called `secrets.js`, in your project
+ * root. This file is included in the .gitignore - it will NOT be tracked
+ * or show up on Github. On your production server, you can add these
+ * keys as environment variables, so that they can still be read by the
+ * Node process on process.env
  */
+if (process.env.NODE_ENV !== 'production') require('../secrets')
 
-// Load environmental variables from .env file
-require("dotenv").config();
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id))
 
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const helmet = require("helmet");
-const compression = require("compression");
-
-// Utilities;
-const createLocalDatabase = require("./utils/createLocalDatabase");
-const seedDatabase = require("./utils/seedDatabase");
-
-// Our database instance;
-const db = require("./database");
-
-// A helper function to sync our database;
-const syncDatabase = () => {
-  if (process.env.NODE_ENV === "production") {
-    db.sync();
-  } else {
-    console.log("As a reminder, the forced synchronization option is on");
-    db.sync({ force: true })
-      .then(() => seedDatabase())
-      .catch((err) => {
-        if (err.name === "SequelizeConnectionError") {
-          createLocalDatabase();
-          seedDatabase();
-        } else {
-          console.log(err);
-        }
-      });
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id)
+    done(null, user)
+  } catch (err) {
+    done(err)
   }
-};
+})
 
-// Instantiate our express application;
-const app = express();
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
 
-// A helper function to create our app with configurations and middleware;
-const configureApp = () => {
-  app.use(helmet());
-  app.use(logger("dev"));
-  // handle request data:
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(compression());
-  app.use(cookieParser());
+  // body parsing middleware
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
 
-  // Our apiRouter
-  const apiRouter = require("./routes/index");
+  // compression middleware
+  app.use(compression())
 
-  // Mount our apiRouter
-  app.use("/api", apiRouter);
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false
+    })
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
 
-  // Error handling;
+  // auth and api routes
+  app.use('/auth', require('./server/auth'))
+  app.use('/api', require('./server/api'))
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
-      const err = new Error("Not found");
-      err.status = 404;
-      next(err);
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
     } else {
-      next();
+      next()
     }
-  });
+  })
 
-  // More error handling;
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
+  // error handling endware
   app.use((err, req, res, next) => {
-    console.error(err);
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || "Internal server error.");
-  });
-};
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
 
-// Main function declaration;
-const bootApp = async () => {
-  await syncDatabase();
-  await configureApp();
-};
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  )
 
-// Main function invocation;
-bootApp();
+  // set up our socket control center
+  const io = socketio(server)
+  require('./server/socket')(io)
+}
 
-// Export our app, so that it can be imported in the www file;
-module.exports = app;
+const syncDb = () => db.sync()
+
+async function bootApp() {
+  await sessionStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
