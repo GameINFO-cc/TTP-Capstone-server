1f2eb4ca635139234505a764498f7a04e0bfcb7c
Rehan Khan <rehangh8924@gmail.com>
2020-06-21 09:58:00 -0400

    edited users model

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const db = require('./server/db')
 const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
-const socketio = require('socket.io')
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -100,9 +99,9 @@ const startListening = () => {
     console.log(`Mixing it up on port ${PORT}`)
   )
 
-  // set up our socket control center
-  const io = socketio(server)
-  require('./server/socket')(io)
+//   // set up our socket control center
+//   const io = socketio(server)
+//   require('./server/socket')(io)
 }
 
 const syncDb = () => db.sync()
